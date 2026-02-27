77207ff725d06b8664f64953b9d2bfea01cd5aac
Kelyan-CABO-FARELO <kelyan.cf@gmail.com>
2026-02-27 16:50:12 +0100

    add apiConstant

---
+++ src/constants/apiConstant.js
@@ -0,0 +1,18 @@
+// ======================
+// CONSTANTES DE L'API
+// ======================
+// ce fichier centralise toutes les URL le l'API Symfony
+// l'avantage: Modifier l'URL de base en un seul endroit
+
+// URL racine du serveur backend
+export const API_ROOT = "http://localhost:8088";
+
+// l'URL de base de l'API Plateform
+export const API_URL = `${API_ROOT}/api`;
+
+// =================================
+// URLs DES RESSOURCES STATIQUES
+// =================================
+
+// Images générales des products
+export const IMAGE_URL = `${API_ROOT}/images`;
